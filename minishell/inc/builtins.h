04ead5490a6ab23f2031f7b1ddcce16d9edef3d4
Hamdy Azali <hazali@f6r2s1.clusters.42paris.fr>
2026-02-17 09:59:55 +0100

    v1

--- minishell/inc/builtins.h
+++ minishell/inc/builtins.h
@@ -6,25 +6,26 @@
 /*   By: hazali <hazali@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/15 02:29:19 by hazali            #+#    #+#             */
-/*   Updated: 2026/02/16 08:16:41 by hazali           ###   ########.fr       */
+/*   Updated: 2026/02/16 09:33:24 by hazali           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTINS_H
-#define BUILTINS_H
+# define BUILTINS_H
 
-#include "minishell.h"
+typedef struct s_minishell	t_minishell;
 
-int ft_is_builtin(char *cmd);
 
-int ft_exec_builtin(char **args, t_minishell *shell);
+int	ft_is_builtin(char *cmd);
 
-int ft_pwd(void);
-int ft_echo(char **args);
-int ft_cd(char **args, t_minishell *shell);
-int ft_export(char **args, t_minishell *shell);
-int ft_unset(char **args, t_minishell *shell);
-int ft_exit(char **args, t_minishell *shell);
-int ft_env(t_minishell *shell);
+int	ft_exec_builtin(char **args, t_minishell *shell);
+
+int	ft_pwd(void);
+int	ft_echo(char **args);
+int	ft_cd(char **args, t_minishell *shell);
+int	ft_export(char **args, t_minishell *shell);
+int	ft_unset(char **args, t_minishell *shell);
+int	ft_exit(char **args, t_minishell *shell);
+int	ft_env(t_minishell *shell);
 
 #endif
